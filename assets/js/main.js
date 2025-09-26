1212da4369138bfa7b60dc064ebf35f8a066b0fa
SuporteWork <mauricio.o@outlook.com>
2025-09-26 13:41:51 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,18 +27,6 @@
   });
 
   /**
-   * Toggle mobile nav dropdowns
-   */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
-    });
-  });
-
-  /**
    * Scroll top button
    */
   const scrollTop = document.querySelector('.scroll-top');
